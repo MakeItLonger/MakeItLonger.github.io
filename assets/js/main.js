69b573466863d2597e976163411147b6f5ac0f3d
MakeItLonger <zavadiuk@gmail.com>
2021-12-22 15:40:14 +0200

    reduce design and generate pdf-file

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,4 +76,42 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
+function scaleCv() {
+    document.body.classList.add("scale-cv");
+  }
+  
+  function removeScale() {
+    document.body.classList.remove("scale-cv");
+  }
+  
+  let areaCv = document.getElementById("area-cv");
+  
+  let resumeButton = document.getElementById("resume-button");
+  
+  let opt = {
+    margin: 0,
+    filename: "zavadiuk_vadym_js_cv.pdf",
+    image: {
+      type: "jpeg",
+      quality: 0.98,
+    },
+    html2canvas: {
+      scale: 4,
+    },
+    jsPDF: {
+      format: "a4",
+      orientation: "portrait",
+    },
+  };
+  
+  function generateResume() {
+    html2pdf(areaCv, opt);
+  }
+  
+  resumeButton.addEventListener("click", () => {
+    scaleCv();
+    generateResume();
+    setTimeout(removeScale, 3000);
+  });
+
 
